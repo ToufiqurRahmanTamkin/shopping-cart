e2250a01dae9bd46aa0aa89311da987661c7cd11
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-01-28 15:24:22 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+document.getElementById('case-increase').addEventListener('click', function(){
+    handleProductChange('case', true);
+});
+
+document.getElementById('case-decrease').addEventListener('click', function(){
+    handleProductChange('case', false);
+});
+
+
+document.getElementById('phone-increase').addEventListener('click', function(){
+    handleProductChange('phone', true);
+});
+
+document.getElementById('phone-decrease').addEventListener('click', function(){
+    handleProductChange('phone', false);
+});
+
+function handleProductChange(product, isIncrease){
+    
+    
+    const productCount = getInputValue(product);
+    let productNewCount = productCount;
+    if(isIncrease == true){
+        productNewCount = productCount + 1;
+    }
+    if(isIncrease == false && productCount > 0){
+        productNewCount = productCount - 1;
+    }
+    document.getElementById(product + '-count') .value = productNewCount;
+    let productTotal = 0;
+    if(product == 'phone'){
+        productTotal = productNewCount * 1219;
+    }
+    if(product == 'case'){
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product+'-total').innerText = '$' + productTotal;
+    calculateTotal();
+}
+
+function calculateTotal(){
+    const phoneCount  = getInputValue('phone');
+    const caseCount = getInputValue('case');
+    const totalPrice = phoneCount * 1219 + caseCount * 59;
+    document.getElementById('total-price').innerText = '$'+ totalPrice;
+
+    const tax = Math.round(totalPrice*0.1);
+    document.getElementById('tax-amount').innerText = '$' + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$'+grandTotal;
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
